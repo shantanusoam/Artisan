30b0d57807d528963e8e60709a98ba7cac618f61
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-01-16 15:36:37 +0530

    send

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -15,29 +15,29 @@ const Blog = () => (
         <Article
           imgUrl={blog01}
           date="Sep 26, 2021"
-          text="ArtisanDAO and Open  AI is the future. Let us exlore how it is?"
+          text="ArtisanDAO is the future. Let us exlore how it is?"
         />
       </div>
       <div className="gpt3__blog-container_groupB">
         <Article
           imgUrl={blog02}
           date="Sep 26, 2021"
-          text="ArtisanDAO and Open  AI is the future. Let us exlore how it is?"
+          text="ArtisanDAO is the future. Let us exlore how it is?"
         />
         <Article
           imgUrl={blog03}
           date="Sep 26, 2021"
-          text="ArtisanDAO and Open  AI is the future. Let us exlore how it is?"
+          text="ArtisanDAO is the future. Let us exlore how it is?"
         />
         <Article
           imgUrl={blog04}
           date="Sep 26, 2021"
-          text="ArtisanDAO and Open  AI is the future. Let us exlore how it is?"
+          text="ArtisanDAO is the future. Let us exlore how it is?"
         />
         <Article
           imgUrl={blog05}
           date="Sep 26, 2021"
-          text="ArtisanDAO and Open  AI is the future. Let us exlore how it is?"
+          text="ArtisanDAO is the future. Let us exlore how it is?"
         />
       </div>
     </div>
